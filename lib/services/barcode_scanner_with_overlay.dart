8f8fb7ff32b915ddbade9871a4f616a4f96d14dd
Arda Fikret Azakli <ardafikretazakli@gmail.com>
2024-10-21 13:48:45 +0200

    gradle updated

---
+++ lib/services/barcode_scanner_with_overlay.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BarcodeScannerWithOverlay extends StatelessWidget {
+  const BarcodeScannerWithOverlay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
